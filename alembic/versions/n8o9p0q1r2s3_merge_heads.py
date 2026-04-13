326e07e55d7bb7bed4a2e4a40d0e7ada48b9ddcb
Parth Kher <kherparth32@gmail.com>
2026-04-13 22:36:26 +0530

    add merge migration from multiple

---
+++ alembic/versions/n8o9p0q1r2s3_merge_heads.py
@@ -0,0 +1,23 @@
+"""Merge heads: hero_sliders and menu_items_nutritional_info
+
+Revision ID: n8o9p0q1r2s3
+Revises: 2a775df5ee3e, m7n8o9p0q1r2
+Create Date: 2026-04-13
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers
+revision = "n8o9p0q1r2s3"
+down_revision = ("2a775df5ee3e", "m7n8o9p0q1r2")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
